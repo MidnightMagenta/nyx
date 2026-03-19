fc6ea28debf5d4768e6f6546db7a78b7dd2fada5
MidnightMagenta <antekswiergocki@gmail.com>
2026-03-19 16:42:03 +0100

    lib: add memset()

---
+++ lib/memset.c
@@ -0,0 +1,7 @@
+#include <string.h>
+
+void *memset(void *ptr, int v, size_t num) {
+    unsigned char *strPtr = (unsigned char *) ptr;
+    while (num--) { *strPtr++ = (unsigned char) v; }
+    return ptr;
+}
