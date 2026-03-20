a909265892c9273a4b81d6239b72a042b31541b5
MidnightMagenta <antekswiergocki@gmail.com>
2026-03-21 00:54:40 +0100

    mm: add bitmap type to pmm.c

--- mm/pmm.c
+++ mm/pmm.c
@@ -1,6 +1,13 @@
+#include <mm/memblock.h>
 #include <mm/memmap.h>
 #include <mm/pmm.h>
 #include <nyx/linkage.h>
 #include <nyx/types.h>
+
+typedef unsigned long bm_word_t;
+
+#define BMP_WORD_BITS (sizeof(bm_word_t) * 8)
+
+static bm_word_t *bm;
 
 void __init __do_pm_init(const struct memmap *memmap) {}
