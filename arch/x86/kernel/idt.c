fbae6c451a4918daac337e71abe5ed8015ce9004
MidnightMagenta <antekswiergocki@gmail.com>
2026-04-26 02:20:58 +0200

    kernel: remove dead code from idt.c

--- arch/x86/kernel/idt.c
+++ arch/x86/kernel/idt.c
@@ -8,24 +8,6 @@
 #define IDT_ENTRIES  256
 #define IDT_TAB_SIZE IDT_ENTRIES * sizeof(gate_desc)
 
-
-#define DEFAULT_STACK 0
-
-#define GATE(_vector, _addr, _ist, _type, _dpl, _segment)                                                              \
-    {                                                                                                                  \
-            .vector    = _vector,                                                                                      \
-            .segment   = _segment,                                                                                     \
-            .addr      = _addr,                                                                                        \
-            .bits.ist  = _ist,                                                                                         \
-            .bits.zero = 0,                                                                                            \
-            .bits.type = _type,                                                                                        \
-            .bits.dpl  = _dpl,                                                                                         \
-            .bits.p    = 1,                                                                                            \
-    }
-
-#define INTG(_vector, _addr) GATE(_vector, _addr, 0, GATE_INTERRUPT, DPL0, 0x08)
-#define TRPG(_vcetor, _addr) GATE(_vector, _addr, 0, GATE_TRAP, DPL0, 0x08)
-
 extern struct idt_data ISR_TABLE_NAME[256];
 
 gate_desc __page_aligned_bss idt[IDT_ENTRIES];
