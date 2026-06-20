75e3a37e69be1f8de7969caf1d4ad656e2938bc6
MidnightMagenta <antekswiergocki@gmail.com>
2026-06-20 04:29:04 +0200

    kernel/x86: add irq state managment functions

--- arch/x86/include/asi/irq.h
+++ arch/x86/include/asi/irq.h
@@ -2,7 +2,29 @@
 #define _ASI_ISR_H
 
 #include <nyx/irq.h>
+#include <nyx/types.h>
+
+#include <asi/system.h>
 
 unsigned int vector_to_irq(unsigned int vector);
+
+static inline void arch_irq_disable() {
+    cli();
+}
+
+static inline void arch_irq_enable() {
+    sti();
+}
+
+static inline flags_t arch_irq_save() {
+    flags_t flags;
+    asm volatile("pushf; pop %0; cli"
+                 : "=r"(flags));
+    return flags;
+}
+
+static inline void arch_irq_restore(flags_t flags) {
+    asm volatile("push %0; popf" ::"r"(flags));
+}
 
 #endif
